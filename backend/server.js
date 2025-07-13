cc604da472000744cca015e3292aa0e70a30d181
Suraj Maity <60690636+surajmaity1@users.noreply.github.com>
2025-07-14 00:34:38 +0530

    Revert "swagger added"

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,8 @@ import { router } from "./routes/index.js";
 import { middleware } from "./middlewares/index.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec, swaggerUICss } from "./swaggerDocs.js";
 
 const app = express();
-
-app.use(
-  "/api/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, {
-    customCss:
-      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
-    customCssUrl: swaggerUICss,
-  })
-);
-
 dotenv.config();
 
 const PORT = process.env.SERVER_PORT;
@@ -28,19 +15,14 @@ app.get("/health", (req, res) => {
 middleware(app);
 app.use("/api", router);
 
-mongoose
-  .connect(`${process.env.MONGODB_URL}`)
-  .then(() => {
-    console.log("Mongodb Connected ... ");
-    app.listen(PORT, () => {
-      console.log(
-        `Server is up and running 🎉 on port http://localhost:${PORT}.`
-      );
-    });
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
+mongoose.connect(`${process.env.MONGODB_URL}`).then(() => {
+  console.log('Mongodb Connected ... ');
+  app.listen(PORT, () => {
+    console.log(`Server is up and running 🎉 on port http://localhost:${PORT}.`);
   });
+}).catch((err) => {
+  console.log("Cannot connect to the database!", err);
+  process.exit();
+});
 
 export default app;
