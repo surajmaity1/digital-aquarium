5574d8f7084f0664c070c38463138894538dc59e
Yash Devani <yashdewani159@gmail.com>
2025-07-14 12:07:41 +0530

    added validator auth.js for Login

--- backend/middlewares/validators/auth.js
+++ backend/middlewares/validators/auth.js
@@ -1,6 +1,6 @@
 import joi from "joi";
 
-export const authValidators = async (req, res, next) => {
+export const SignUpValidators = async (req, res, next) => {
    const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&])/;
     const schema = joi
         .object()
@@ -26,6 +26,35 @@ export const authValidators = async (req, res, next) => {
                 "any.required": "gender is required",
                 "string.empty": "gender cannot be empty",
             }),
+
+        });
+    try {
+        await schema.validateAsync(req.body, { abortEarly: false });
+        next();
+    } catch (error) {
+        res.boom.badRequest(error.details[0].message);
+    }
+}
+
+export const loginValidators = async (req, res, next) => {
+   const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&])/;
+    const schema = joi
+        .object()
+        .strict()
+        .keys({
+            email: joi.string().email().required().messages({
+                "string.email": "email must be valid",
+                "string.empty": "email cannot be empty",
+                "any.required": "email is required",
+            }),
+            password: joi.string().min(8).max(30).pattern(passwordRegex).required().messages({
+                "string.min": "password must be at least 8 characters long",
+                "string.max": "password must be at most 30 characters long",
+                "string.pattern.base": "password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+                "string.empty": "password cannot be empty",
+                "any.required": "password is required",
+            }),
+         
 
         });
     try {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { registerUser, loginUser, logoutUser,  resetPassword } from "../controllers/auth.js";
-import { authValidators } from "../middlewares/validators/auth.js";
+import { SignUpValidators, loginValidators } from "../middlewares/validators/auth.js";
 
 const router = express.Router();
 
-router.post('/register', authValidators, registerUser);
-router.post('/login', authValidators, loginUser);
+router.post('/register', SignUpValidators, registerUser);
+router.post('/login', loginValidators, loginUser);
 router.get('/logout', logoutUser);
 router.post('/reset-password', resetPassword);
 
